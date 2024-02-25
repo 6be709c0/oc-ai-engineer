3c03236ca092c30e8b033f1e2e4a8cd6b06435b8
Math <mathieu@lescaudron.com>
2024-02-25 01:29:18 +0100

    Save progression

--- OC/P8/exercise/segmentation_from_scratch/dataset.py
+++ OC/P8/exercise/segmentation_from_scratch/dataset.py
@@ -2,6 +2,8 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
+import matplotlib.pyplot as plt  
+from torchvision.transforms import ToPILImage  # To convert tensors to PIL images  
 
 class CarvanaDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
@@ -18,11 +20,23 @@ class CarvanaDataset(Dataset):
         mask_path = os.path.join(self.mask_dir, self.images[index].replace(".jpg", "_mask.gif"))
         image = np.array(Image.open(img_path).convert("RGB"))
         mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        # Get unique values and their counts  
+        unique, counts = np.unique(mask, return_counts=True)  
+        print("MASK", unique, counts)
+        
         mask[mask == 255.0] = 1.0
+        unique, counts = np.unique(mask, return_counts=True)  
+        print("MASK 2", unique, counts)
 
         if self.transform is not None:
             augmentations = self.transform(image=image, mask=mask)
             image = augmentations["image"]
             mask = augmentations["mask"]
+
+        # to_pil = ToPILImage()  
+        # pil_img = to_pil(image)  
+        # pil_img.save("test.png")
+        # pil_mask = to_pil(mask)  
+        # pil_mask.save("mask.png")
 
         return image, mask
---
+++ OC/P8/output/dataset.py
@@ -0,0 +1,45 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+import matplotlib.pyplot as plt  
+from torchvision.transforms import ToPILImage  # To convert tensors to PIL images  
+import json
+
+from helpers import *
+
+
+class CityScapeDataset(Dataset):
+    def __init__(self, dataframe,  transform=None):
+        self.dataframe = dataframe
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataframe)
+
+    def __getitem__(self, index):
+        img_path = self.dataframe.loc[index]["original_img_path"]
+        mask_path = self.dataframe.loc[index]["polygons_json_path"]
+
+        img = Image.open(img_path).convert("RGB")
+        image_np = np.array(img)
+
+        mask =  Image.fromarray(polygon_to_mask_img(mask_path)).convert("RGBA")
+        mask_np = np.array(mask.convert("L"), dtype=np.float32)
+
+        # unique, counts = np.unique(mask_np, return_counts=True)  
+        # print("MASK", unique, counts)
+    
+        if self.transform is not None:
+            augmentations = self.transform(image=image_np, mask=mask_np)
+            image_np = augmentations["image"]
+            mask_np = augmentations["mask"]
+        
+        # to_pil = ToPILImage()  
+        # pil_img = to_pil(image_np)  
+        # pil_img.save("img.png") 
+        
+        # pil_img = to_pil(mask_np)  
+        # pil_img.save("mask.png") 
+
+        return image_np, mask_np
